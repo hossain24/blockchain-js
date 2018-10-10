2bdabf1628ef5a7a742325712e2cd8d965cbe087
hossain24 <haj.ctg@gmail.com>
2018-10-11 00:43:39 +0200

    check validity

--- main.js
+++ main.js
@@ -37,10 +37,29 @@ class BlockChain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.calculateHash()) {
+        return false;
+      }
+
+      return true;
+    }
+  }
 }
 
 let hillCoin = new BlockChain();
 hillCoin.addBlock(new Block(1, "10/10/2018", { amount: 5 }));
 hillCoin.addBlock(new Block(2, "11/10/2018", { amount: 10 }));
 
-console.log(JSON.stringify(hillCoin, null, 5));
+console.log("Is blockchain valid? " + hillCoin.isChainValid());
+
+//console.log(JSON.stringify(hillCoin, null, 5));
